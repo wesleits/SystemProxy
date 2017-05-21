3639adde310ad2fb5e08a5448bc67fef64716f26
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-05-20 22:44:19 -0300

    5

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("oi");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.JS!');
+}).listen(8080);
+console.log('Server running at http://localhost:8080/');
