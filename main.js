38f504961bfc1f3ba7918b0e5532e95758123c4e
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-05-20 22:40:54 -0300

    4

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-const http = require('http')  
-const port = 666
-
-const requestHandler = (request, response) => {  
-  console.log(request.url)
-  response.end('Teste!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+console.log("oi");
