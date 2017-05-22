bfbd688303c5509611451f21ed67af0314dabe4d
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-05-22 18:05:21 -0300

    teste 26

--- nodejs/files/main.js
+++ nodejs/files/main.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('teste 25!');
+  res.end('teste 26!');
 }).listen(3128); 
 console.log('Server running at http://localhost:80/');
