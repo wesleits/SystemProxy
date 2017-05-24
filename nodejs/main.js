0651b3ff36c4b3ace6988353259ca98eb5ecb750
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-05-24 15:59:32 -0300

    teste 35

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('eu amo a millene!');
+  res.end('teste 35!');
 }).listen(3128); 
 console.log('Server running at http://localhost:80/');
