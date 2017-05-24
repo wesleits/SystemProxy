e07669917b70c66b52e436bdf55d485fffe0f36e
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-05-24 12:45:12 -0300

    teste Millene

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('teste 34!');
+  res.end('eu amo a millene!');
 }).listen(3128); 
 console.log('Server running at http://localhost:80/');
