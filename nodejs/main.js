b6d07e8b9b680d57a7b1c4f08eddd6c790ce29a4
Weslei Teixeira da Silveira <wesleiteixeira@hotmail.com.br>
2017-06-30 17:03:42 -0300

    Update main.js

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,44 +1,9 @@
 var http = require('http');
-var os = require('os');
-
 
 http.createServer(function (req, res) 
 {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-
-	/*
-	var options = {
-		host: 'arenacosplay.tempsite.ws',
-		path: '/ip'
-	}
-	var request = http.request(options, function (cres) {
-		var data = '';
-		cres.on('data', function (chunk) {
-			data += chunk;
-		});
-		cres.on('end', function () {
-			res.end(data);
-
-		});
-	});
-	request.on('error', function (e) {
-		res.end(e.message);
-	});
-	request.end();
-	*/
-	
-	var interfaces = os.networkInterfaces();
-	var addresses = [];
-	for (var k in interfaces) {
-		for (var k2 in interfaces[k]) {
-			var address = interfaces[k][k2];
-			if (address.family === 'IPv4' && !address.internal) {
-				addresses.push(address.address);
-			}
-		}
-	}
-
-	res.end(addresses.toString());
+	res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+  	res.write('Olá Mundo! Essa é a v1');
+	res.end();
 })
-.listen(3128); 
-console.log('Server running!');
+.listen(80);
